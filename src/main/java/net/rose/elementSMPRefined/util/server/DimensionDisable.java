fbd27745105a1c5aabee57fa3a25285691c6a270
1Pumpkinn <wishsmp1002@gmail.com>
2026-09-23 08:39:08 +1000

    just added a comment for later

--- src/main/java/net/rose/elementSMPRefined/util/server/DimensionDisable.java
+++ src/main/java/net/rose/elementSMPRefined/util/server/DimensionDisable.java
@@ -2,6 +2,6 @@ package net.rose.elementSMPRefined.util.server;
 
 public class DimensionDisable {
 
-    // disable any dimension
+    // disable any dimension portal cancel event also make sure entities like ender pearls, boats, horses don't go through / cancel
 
 }
